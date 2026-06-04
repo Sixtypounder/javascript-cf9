87cb9dfaf4dc88cf775c8742277400559589c730
Sixtypounder <kaipanospanagiotis@gmail.com>
2026-06-04 21:30:44 +0300

    feat: add cibtrik structures

---
+++ boolean/for.js
@@ -0,0 +1,16 @@
+let sum = 0;
+
+for (let i = 1; i <= 10; i++) {
+    sum += i;
+}
+
+
+//Enchanced for
+let numbers = [1, 2, 3, 4, 5];
+
+for (const num of numbers) {
+    sum +- num;
+}
+
+console.log("Sum with enchanced for: ", sum);
+
