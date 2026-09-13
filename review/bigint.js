8e67d27f3ae1239531f9c8b7cd3df916fb3c5c75
Sixtypounder <kaipanospanagiotis@gmail.com>
2026-09-13 13:22:35 +0300

    chore: add review

---
+++ review/bigint.js
@@ -0,0 +1,8 @@
+/**
+ * Define a bigint var and literal and increase by one and print the result.
+ */
+
+let bigTime = 9007199254740991n;
+bigTime++;
+
+console.log(bigTime)
---
+++ review/strings.js
@@ -0,0 +1,8 @@
+/**
+ * Declare two strings and print with iterpolation
+ */
+
+const firstname = "Alice";
+const lastname = "Johnson";
+
+console.log(`Hello, ${firstname} ${lastname}!`) 
